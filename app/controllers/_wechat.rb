917dd4e7b23398b276fe900ceecf1f18487af090
Lu Jun <luj1985@gmail.com>
2014-04-20 03:22:26 +0800

    add signature post handler

--- app/controllers/_wechat.rb
+++ app/controllers/_wechat.rb
@@ -13,6 +13,8 @@ module Wechat
         app.post :index do
           content_type :xml
           body = request.body.read || ""
+          return "" if params[:signature] # sometimes wechat will post signature to this endpoint
+          
           halt 404, "unknown post body" if body.empty?
 
           doc = Nokogiri::XML(body).root
