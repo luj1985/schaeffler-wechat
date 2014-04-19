ecfe6f7fc1e7cd3c63fb4a4aed8d06c606ee8a7a
Lu Jun <luj1985@gmail.com>
2014-04-20 03:41:04 +0800

    cleanup code

--- app/controllers/_wechat.rb
+++ app/controllers/_wechat.rb
@@ -4,46 +4,8 @@ module Wechat
       def registered app
         event_handlers = Hash.new
 
-        app.get :index do
-          token = ENV["WECHAT_TOKEN"] || 'test'
-          raw = [token, params[:timestamp], params[:nonce]].compact.sort.join
-          Digest::SHA1.hexdigest(raw) == params[:signature] ? params[:echostr] : ""
-        end
+        controller = class << app ;self; end
 
-        app.post :index do
-          content_type :xml
-          body = request.body.read || ""
-          
-          halt 404, "unknown post body" if body.empty?
-
-          doc = Nokogiri::XML(body).root
-          hash = doc.element_children.each_with_object(Hash.new) do |e, h|
-            name = e.name.gsub(/(.)([A-Z])/,'\1_\2').downcase
-            h[name.to_sym] = e.content
-          end
-          type = hash[:msg_type].downcase.to_sym
-          case type
-          when :event
-            event = hash[:event].downcase.to_sym
-            puts "event_handlers #{event_handlers.inspect}"
-            puts "hash #{hash}"
-            handlers = event_handlers[event] || []
-            handler = handlers.find do |handler|
-              handler[:condition].call(hash)
-            end
-            if handler.nil? then
-              puts "handler is not defined"
-              return "" 
-            else 
-              handler[:proc].call(hash)
-            end 
-          else
-            puts "Cannot handle this kind of message #{type}"
-            return ""
-          end
-        end
-
-        controller = class << app; self; end
         controller.instance_eval do
           # TODO: add other message type
           define_method :wechat_event do |event, condition = nil, &blk|
@@ -55,6 +17,38 @@ module Wechat
             event_handlers[event] << task
           end
         end
+
+        app.get :index do
+          token = ENV["WECHAT_TOKEN"] || 'test'
+          raw = [token, params[:timestamp], params[:nonce]].compact.sort.join
+          Digest::SHA1.hexdigest(raw) == params[:signature] ? params[:echostr] : ""
+        end
+
+        app.post :index do
+          content_type :xml
+          body = request.body.read || ""
+          halt 501, "unknown post body" if body.empty?
+
+          doc = Nokogiri::XML(body).root
+          hash = doc.element_children.each_with_object(Hash.new) do |e, h|
+            name = e.name.gsub(/(.)([A-Z])/,'\1_\2').downcase
+            h[name.to_sym] = e.content
+          end
+          type = hash[:msg_type].downcase.to_sym
+          case type
+          when :event
+            event = hash[:event].downcase.to_sym\
+            handlers = event_handlers[event] || []
+            handler = handlers.find do |handler|
+              handler[:condition].call(hash)
+            end
+            halt 501, "no handler defined" if handler.nil?
+            handler[:proc].call(hash)
+          else
+            halt 501, "unknown event type #{type}"
+          end
+        end
+
       end
       alias :included :registered
     end
