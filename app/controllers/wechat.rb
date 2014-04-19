7b9c200b481239e2c6a3cc8f037cf525e409c112
Lu Jun <luj1985@gmail.com>
2014-04-19 12:48:46 +0800

    cleanup code

--- app/controllers/wechat.rb
+++ app/controllers/wechat.rb
@@ -1,38 +1,14 @@
-require 'digest'
-
-SchaefflerWechat::App.controllers :wechat do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
+SchaefflerWechat::App.controllers :wechat do  
   get :index do
     signature = params[:signature]
     timestamp = params[:timestamp]
     nonce = params[:nonce]
-    echostr = params[:echostr]
-    token = ENV["token"] || 'test'
 
-    puts token
+    token = ENV["token"] || 'test'
 
     raw = [token, timestamp, nonce].compact.sort.join
     hash = Digest::SHA1.hexdigest(raw)
 
-    hash == signature ? echostr : ""
+    hash == signature ? params[:echostr] : ""
   end
 end
