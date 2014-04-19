99fc3e99da86c2009e175efd25f421e3e5cb53e5
Lu Jun <luj1985@gmail.com>
2014-04-20 02:07:35 +0800

    add unicorn runtime required folder

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,11 +9,10 @@ pid "/var/www/schaeffler-wechat/pids/unicorn.pid"
 # Path to logs
 # stderr_path "/path/to/log/unicorn.log"
 # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/var/www/schaeffler-wechat/log/unicorn.log"
-stdout_path "/var/www/schaeffler-wechat/log/unicorn.log"
+stderr_path "/var/www/schaeffler-wechat/log/unicorn.err.log"
+stdout_path "/var/www/schaeffler-wechat/log/unicorn.out.log"
 
 # Unicorn socket
-listen "/tmp/unicorn.schaeffler-wechat.sock"
 listen "/tmp/unicorn.schaeffler-wechat.sock"
 
 # Number of processes
