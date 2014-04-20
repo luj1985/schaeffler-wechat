017db92bc8dfc6c433b26311caadbea707ef9d55
Lu Jun <luj1985@gmail.com>
2014-04-20 12:20:50 +0800

    reduce unicorn worker process

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,23 +1,12 @@
-# Set the working application directory
-# working_directory "/path/to/your/app"
 working_directory "/var/www/schaeffler-wechat"
 
-# Unicorn PID file location
-# pid "/path/to/pids/unicorn.pid"
 pid "/var/www/schaeffler-wechat/pids/unicorn.pid"
 
-# Path to logs
-# stderr_path "/path/to/log/unicorn.log"
-# stdout_path "/path/to/log/unicorn.log"
 stderr_path "/var/www/schaeffler-wechat/log/unicorn.err.log"
 stdout_path "/var/www/schaeffler-wechat/log/unicorn.out.log"
 
-# Unicorn socket
 listen "/tmp/unicorn.schaeffler-wechat.sock"
 
-# Number of processes
-# worker_processes 4
-worker_processes 2
+worker_processes 1
 
-# Time-out
 timeout 30
