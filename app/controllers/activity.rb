3680e745ae8ca2094d1bf7f070744bf761c5c356
Lu Jun <luj1985@gmail.com>
2014-04-20 15:19:50 +0800

    add little themes

--- app/controllers/activity.rb
+++ app/controllers/activity.rb
@@ -1,28 +1,7 @@
 SchaefflerWechat::App.controllers :activity do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-
-  
   get :index, :with => :openid do
     openid = session[:openid] = params[:openid]
-    render :home
+    render :index
   end
 
   post :challenge do
