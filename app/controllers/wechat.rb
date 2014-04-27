ddcad002cf18e49c5abd939c834c6b148287f1ac
Lu Jun <luj1985@gmail.com>
2014-04-28 00:13:03 +0800

    use instance_exec to run block

--- app/controllers/wechat.rb
+++ app/controllers/wechat.rb
@@ -1,7 +1,50 @@
-require '_wechat'
-
 SchaefflerWechat::App.controllers :wechat do
-  register Wechat::Events
+  configure do
+    set :event_handlers, {}
+  end
+
+  def wechat_event event, condition = nil, &blk
+    event_handlers = settings.event_handlers
+    event_handlers[event] ||= Array.new
+    task = {:proc => blk}
+    task.merge!(:condition => lambda {|hash|
+      condition.all? { |k, v| condition[k] == v }
+    }) if condition.present?
+    event_handlers[event] << task
+  end
+
+
+  get :index do
+    token = ENV["WECHAT_TOKEN"] || 'test'
+    raw = [token, params[:timestamp], params[:nonce]].compact.sort.join
+    Digest::SHA1.hexdigest(raw) == params[:signature] ? params[:echostr] : ""
+  end
+
+  post :index do
+    content_type :xml
+    event_handlers = settings.event_handlers
+    body = request.body.read || ""
+    # TODO validate post message signature
+    halt 501, "unknown post body" if body.empty?
+
+    doc = Nokogiri::XML(body).root
+    hash = doc.element_children.each_with_object(Hash.new) do |e, h|
+      name = e.name.gsub(/(.)([A-Z])/,'\1_\2').downcase
+      h[name.to_sym] = e.content
+    end
+    type = hash[:msg_type].downcase.to_sym
+    case type
+    when :event
+      event = hash[:event].downcase.to_sym
+      handlers = event_handlers[event] || []
+      handler = handlers.find { |h| h[:condition].call(hash) }
+      halt 501, "no handler defined" if handler.nil?
+      instance_exec hash, &handler[:proc]
+    else
+      halt 501, "unknown event type #{type}"
+    end
+  end
+
 
   wechat_event :click, :event_key => 'activity' do |hash|
     host = ENV['WECHAT_HOST']
@@ -14,14 +57,14 @@ SchaefflerWechat::App.controllers :wechat do
         xml.ArticleCount 2
         xml.Articles {
           xml.item {
-            xml.Title "Introduction"
-            xml.Description "Activity introduction description"
+            xml.Title t('activity.intro.title')
+            xml.Description t('activity.intro.description')
             xml.PicUrl URI.join(host, '/images/introduction.jpg')
             xml.Url URI.join(host, url_for(:activity, :intro, :openid => hash[:from_user_name]))
           }
           xml.item {
-            xml.Title "Enter activity"
-            xml.Description "Lucky draw activity description"
+            xml.Title t('activity.entry.title')
+            xml.Description t('activity.entry.description')
             xml.PicUrl URI.join(host, '/images/innovation_intro_header.jpg')
             xml.Url URI.join(host, url_for(:activity, :index, :openid => hash[:from_user_name]))
           }
@@ -30,4 +73,5 @@ SchaefflerWechat::App.controllers :wechat do
     end
     builder.to_xml
   end
+
 end
