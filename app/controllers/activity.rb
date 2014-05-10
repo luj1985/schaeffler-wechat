b08918469aea7df18bad9bed5ed9dab46e737e1e
Lu Jun <luj1985@gmail.com>
2014-05-10 22:13:56 +0800

    update messages

--- app/controllers/activity.rb
+++ app/controllers/activity.rb
@@ -24,26 +24,14 @@ SchaefflerWechat::App.controllers :activity, :conditions => {:protect => true} d
     raw = params[:lottery][:serial]
     serial = raw.scan(/\d/).join('')
     @lottery = Lottery.challenge serial
-    if @lottery then # correct serial number
-      if @lottery.available? then
-        @lottery.status = 'EXCHANGING'
-        user = User.find_or_initialize_by :openid => session[:openid]
-        @lottery.user = user
-        @lottery.serial = serial
-        render :confirm
-      else
-        @message = <<EOF
-<p class="message">您所输入的兑奖验证码已经被兑换。请您检查是否验证码输入有误。</p>
-<p>如有问题，请拨打活动热线：<a href="tel:021-39576702">021-39576702</a></p>
-EOF
-        render :message
-      end
+    if @lottery && @lottery.available?then # correct serial number
+      @lottery.status = 'EXCHANGING'
+      user = User.find_or_initialize_by :openid => session[:openid]
+      @lottery.user = user
+      @lottery.serial = serial
+      render :confirm
     else
-      @message = <<EOF
-<p class="message">您输入的验证码有误，请您检查并重新输入。</p>
-<p>如有问题，请拨打活动热线：<a href="tel:021-39576702">021-39576702</a></p>
-EOF
-      render :message
+      render :fail
     end
   end
 
@@ -80,8 +68,7 @@ EOF
   post :apply do
     misses = params.reject do |id, value|
       question = Question.find_by_id id
-      return false unless question
-      question.correct == value
+      question && question.correct == value
     end
     user = User.find_by_openid session[:openid]
     halt 500, "没有找您的微信账户" unless user
@@ -91,25 +78,11 @@ EOF
       return render :message
     end
 
-    passed = (params.length == 2) && misses.empty?
+    @passed = (params.length == 2) && misses.empty?
     user.apply_attemped = true
-    user.join_match = passed
+    user.join_match = @passed
     user.save
-    msg_passed = <<EOF
-<p class="message">恭喜您回答正确！</p>
-<p class="message">
-您获得了申请现场观赛的资格，我们将于8月初和10月初
-从回答正确的申请者中抽取出幸运者！获得现场激情观赛的机会哦！</p>
-<p class="message">届时请您关注舍弗勒汽车售后微信公共帐号！</p>
-EOF
-    msg_fail = <<EOF
-<p class="message">非常抱歉，您提交的答案不正确！
-<img src="/images/cry.gif" style="height:50px;width:50px"/>
-</p>
-<p class="message">预祝您能刮中更多奖品哦！</p>
-EOF
-    @message = passed ? msg_passed : msg_fail
-    render :message
+    render :answer
   end
 
 
