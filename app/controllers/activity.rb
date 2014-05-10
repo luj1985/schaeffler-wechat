af865cf9341ec0ae15e4b488652acf66bfe18326
Lu Jun <luj1985@gmail.com>
2014-05-10 17:29:44 +0800

    remove external locale text dependency

--- app/controllers/activity.rb
+++ app/controllers/activity.rb
@@ -40,7 +40,7 @@ SchaefflerWechat::App.controllers :activity, :conditions => {:protect => true} d
     halt 500 unless user
 
     if user.apply_attemped then
-      @message = t('activity.apply.attempted')
+      @message = '不能多次申请观赛'
       return render :message
     end
 
@@ -48,7 +48,20 @@ SchaefflerWechat::App.controllers :activity, :conditions => {:protect => true} d
     user.apply_attemped = true
     user.join_match = passed
     user.save
-    @message = passed ? t('activity.question.pass') : t('activity.question.fail')
+    msg_passed = <<EOF
+<p class="message">恭喜您回答正确！</p>
+<p class="message">
+您获得了申请现场观赛的资格，我们将于8月初和10月初
+从回答正确的申请者中抽取出幸运者！获得现场激情观赛的机会哦！</p>
+<p class="message">届时请您关注舍弗勒汽车售后微信公共帐号！</p>
+EOF
+    msg_fail = <<EOF
+<p class="message">非常抱歉，您提交的答案不正确！
+<img src="/images/cry.gif" style="height:50px;width:50px"/>
+</p>
+<p class="message">预祝您能刮中更多奖品哦！</p>
+EOF
+    @message = passed ? msg_passed : msg_fail
     render :message
   end
 
@@ -57,8 +70,15 @@ SchaefflerWechat::App.controllers :activity, :conditions => {:protect => true} d
     serial = raw.scan(/\d/).join('')
   	crypted_serial = Digest::MD5::hexdigest serial
   	@lottery = Lottery.find_by_crypted_serial crypted_serial
-    if @lottery then
-      if not @lottery.available? then
+    if @lottery then # correct serial number
+      if @lottery.available? then
+        @lottery.status = 'EXCHANGING'
+        openid = session[:openid]
+        user = User.find_or_initialize_by :openid => openid
+        @lottery.user = user
+        @lottery.serial = serial
+        render :confirm
+      else
         @message = <<EOF
 <p class="message">对不起，您输入的验证码已经被兑换。</p>
 <p>如有问题，您可拨打活动专线咨询：
@@ -66,14 +86,6 @@ SchaefflerWechat::App.controllers :activity, :conditions => {:protect => true} d
 </p>
 EOF
         render :message
-      else
-        @lottery.status = 'EXCHANGING'
-        # TODO: should redirect page if open id is not defined
-        openid = session[:openid]
-        user = User.find_or_initialize_by :openid => openid
-        @lottery.user = user
-        @lottery.serial = serial
-        render :confirm
       end
     else
       @message = <<EOF
@@ -91,9 +103,18 @@ EOF
     halt 404 unless @lottery
     @lottery.status = 'USED'
     if @lottery.update(params[:lottery])
-      @message = @lottery.level == '4' ? 
-        t('activity.result.success4')  : 
-        t('activity.result.success')
+      l4_succss_msg = <<EOF
+<p class="message">您的兑奖信息已经提交成功</p>
+<p class="message">根据您提交的信息我们将在三个工作日内为您指定的手机号码充值，请您关注。</p>
+<p class="message">感谢您对舍弗勒的支持与信赖。</p>
+EOF
+      success_msg = <<EOF
+<p class="message">您的兑奖信息已经提交成功</p>
+<p class="message">根据您提交的信息我们的工作人员将于一周以内联系您。</p>
+<p class="message">感谢您对舍弗勒的支持与信赖。</p>
+EOF
+      success_msg += "<a href=#{url_for(:activity, :rules)}>申请观赛</a>" if @lottery.can_apply_join_match?
+      @message = @lottery.level == '4' ? l4_succss_msg : success_msg
       render :message
     else
       render :confirm
