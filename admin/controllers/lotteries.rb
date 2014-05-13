6fe65e7c60bec308eb468bf4a4211efc963de162
Lu Jun <luj1985@gmail.com>
2014-05-13 23:00:40 +0800

    update excel export

--- admin/controllers/lotteries.rb
+++ admin/controllers/lotteries.rb
@@ -16,17 +16,21 @@ SchaefflerWechat::Admin.controllers :lotteries do
     attachment "已兑奖号码#{timestamp}.xlsx"
     Axlsx::Package.new do |p|
       p.workbook.add_worksheet(:name => "已被兑奖号码 #{timestamp}") do |sheet|
-        sheet.add_row ["验证码","奖品","产品号","充值号码","OpenID","用户名","联系电话","省份","城市","详细地址","修理厂名"]
+        sheet.add_row [
+          "验证码","奖品","奖品等级", "产品号","充值号码", 
+          "用户名","联系电话","省份","城市","详细地址","修理厂名"
+        ]
         @lotteries.each do |l|
           u = l.user
-          row = [ l.serial, l.display_name, l.product, l.tel, 
+          row = [ 
+            l.serial, l.display_name, l.level_name, l.product, l.tel, 
             u.name, u.tel, u.province, u.city, u.workshop_address, u.workshop
           ]
           types = row.map {|v| :string }
           sheet.add_row row, :types => types
         end
       end
-      s = p.to_stream()
+      s = p.to_stream
       response.write(s.read)
     end
   end
--- admin/controllers/users.rb
+++ admin/controllers/users.rb
@@ -10,7 +10,11 @@ SchaefflerWechat::Admin.controllers :users do
     attachment "参与用户#{timestamp}.xlsx"
     Axlsx::Package.new do |p|
       p.workbook.add_worksheet(:name => "参与用户 #{timestamp}") do |sheet|
-        sheet.add_row ["OpenID","用户名","联系电话","提交观赛申请", "申请结果","省份","城市","详细地址","修理厂名"]
+        sheet.add_row [
+          "OpenID","用户名","联系电话",
+          "提交观赛申请", 
+          "申请结果",
+          "省份","城市","详细地址","修理厂名"]
         User.all.each do |u|
           row = [u.openid, u.name, u.tel, 
             u.apply_attemped ? "是" : "否",
@@ -20,7 +24,7 @@ SchaefflerWechat::Admin.controllers :users do
           sheet.add_row row, :types => types
         end
       end
-      s = p.to_stream()
+      s = p.to_stream
       response.write(s.read)
     end
   end
