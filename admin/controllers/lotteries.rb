7c30bd5c02afc6a73524f27320d21a22726d6f5a
Lu Jun <luj1985@gmail.com>
2014-05-13 22:55:02 +0800

    add excel export

--- admin/controllers/lotteries.rb
+++ admin/controllers/lotteries.rb
@@ -12,25 +12,22 @@ SchaefflerWechat::Admin.controllers :lotteries do
 
   get :export, :provides => [:csv, :xlsx] do
     @lotteries = Lottery.includes(:user).where("serial is not null")
-    csv_string = CSV.generate(:force_quotes => true) do |csv|  
-      csv << ["验证码","奖品","产品号","充值号码","OpenID","用户名","联系电话","省份","城市","详细地址","修理厂名"]  
-      @lotteries.each do |l|
-        user = l.user
-        csv << [
-          l.serial,
-          l.display_name,
-          l.product,
-          l.tel,
-          user.openid, 
-          user.name,
-          user.tel,
-          user.province,
-          user.city,
-          user.workshop_address,
-          user.workshop
-        ]  
-      end  
-    end 
-    csv_string
+    timestamp = Time.now.strftime "%Y-%m-%d %H%M%S"
+    attachment "已兑奖号码#{timestamp}.xlsx"
+    Axlsx::Package.new do |p|
+      p.workbook.add_worksheet(:name => "已被兑奖号码 #{timestamp}") do |sheet|
+        sheet.add_row ["验证码","奖品","产品号","充值号码","OpenID","用户名","联系电话","省份","城市","详细地址","修理厂名"]
+        @lotteries.each do |l|
+          u = l.user
+          row = [ l.serial, l.display_name, l.product, l.tel, 
+            u.name, u.tel, u.province, u.city, u.workshop_address, u.workshop
+          ]
+          types = row.map {|v| :string }
+          sheet.add_row row, :types => types
+        end
+      end
+      s = p.to_stream()
+      response.write(s.read)
+    end
   end
 end
--- admin/controllers/users.rb
+++ admin/controllers/users.rb
@@ -5,22 +5,23 @@ SchaefflerWechat::Admin.controllers :users do
     render 'users/index'
   end
 
-  get :export, :provides => [:csv, :xlsx] do
-    csv_string = CSV.generate(:force_quotes => true) do |csv|  
-      csv << ["OpenID","用户名","联系电话","申请观赛","省份","城市","详细地址","修理厂名"]  
-      User.all.each do |u|
-        csv << [
-          u.openid, 
-          u.name,
-          u.tel,
-          u.join_match ? "申请观赛成功" : "",
-          u.province,
-          u.city,
-          u.workshop_address,
-          u.workshop
-        ]
-      end 
+  get :export, :provides => :xlsx do
+    timestamp = Time.now.strftime "%Y-%m-%d %H%M%S"
+    attachment "参与用户#{timestamp}.xlsx"
+    Axlsx::Package.new do |p|
+      p.workbook.add_worksheet(:name => "参与用户 #{timestamp}") do |sheet|
+        sheet.add_row ["OpenID","用户名","联系电话","提交观赛申请", "申请结果","省份","城市","详细地址","修理厂名"]
+        User.all.each do |u|
+          row = [u.openid, u.name, u.tel, 
+            u.apply_attemped ? "是" : "否",
+            u.join_match ? "成功" : u.apply_attemped ? "失败" : "", 
+            u.province, u.city, u.workshop_address, u.workshop ]
+          types = row.map {|v| :string }
+          sheet.add_row row, :types => types
+        end
+      end
+      s = p.to_stream()
+      response.write(s.read)
     end
-    csv_string
   end
 end
