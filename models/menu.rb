b6ca7e6c689f6963a6dc4c18d6479fddc308067a
Lu Jun <luj1985@gmail.com>
2014-05-24 17:18:39 +0800

    update menu

--- models/menu.rb
+++ models/menu.rb
@@ -1,4 +1,7 @@
 class Menu < ActiveRecord::Base
   has_many :relations
   has_many :articles, :through => :relations
+
+  validates :name, :presence => true, :uniqueness => true
+  validates :title, :presence => true
 end
