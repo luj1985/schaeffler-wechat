fbb2c655493c09fc9cd6ae8940a3453a488eaaed
Lu Jun <luj1985@gmail.com>
2014-06-02 18:02:29 +0800

    add uniq validation

--- models/article.rb
+++ models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :name, uniqueness: true
   validates :title, presence: true
   validates :body, presence: true
--- models/menu.rb
+++ models/menu.rb
@@ -1,12 +1,12 @@
 class Menu < ActiveRecord::Base
   attr_accessor :selected_articles, :selected_pages
   
+  validates :name, :presence => true, :uniqueness => true
+  validates :title, :presence => true
+
   has_many :relations
   has_many :articles, :through => :relations
 
   has_many :page_relations
   has_many :pages, :through => :page_relations
-
-  validates :name, :presence => true, :uniqueness => true
-  validates :title, :presence => true
 end
