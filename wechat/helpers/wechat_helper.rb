dc20123a3df8684c8f3d1d53b188cc47d37327e7
Lu, Jun <luj1985@gmail.com>
2014-06-17 21:54:24 +0800

    add message validation

--- wechat/helpers/wechat_helper.rb
+++ wechat/helpers/wechat_helper.rb
@@ -1,9 +1,7 @@
 SchaefflerWechat::Wechat.helpers do
-
   def validate_messages
-#   token = ENV["WECHAT_TOKEN"] || 'test'
-#    raw = [token, params[:timestamp], params[:nonce]].compact.sort.join
-#    Digest::SHA1.hexdigest(raw) == params[:signature]
-    true
+    token = ENV["WECHAT_TOKEN"] || 'test'
+    raw = [token, params[:timestamp], params[:nonce]].compact.sort.join
+    ENV['RACK_ENV'] == 'development' ? true : Digest::SHA1.hexdigest(raw) == params[:signature]    
   end
 end
