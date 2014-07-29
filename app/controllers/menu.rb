d6d05d5fa45fb72efd08ef71a9b1a9fd000d0838
Lu, Jun <luj1985@gmail.com>
2014-07-29 21:27:45 +0800

    add preivew menu

--- app/controllers/menu.rb
+++ app/controllers/menu.rb
@@ -1,8 +1,22 @@
 SchaefflerWechat::App.controllers :menu do
   
+  get :preview do
+    @menu = params[:menu]
+    selected_articles = @menu[:selected_articles]
+    selected_pages = @menu[:selected_pages]
+    articles = Article.find selected_articles unless selected_articles.nil?
+    pages = Page.find selected_pages unless selected_pages.nil?
+
+    @articles = articles || []
+    @pages = pages || []
+    render :index
+  end
+
   get :index, :with => :name do
     # TODO: add menu image parse?
     @menu = Menu.find_by_name params[:name]
+    @articles = @menu.articles
+    @pages = @menu.pages
     render :index
   end
 
