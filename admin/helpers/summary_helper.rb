0467455af251731ca4c888dee8b0462ece9e7bc9
Lu, Jun <luj1985@gmail.com>
2014-09-28 21:36:03 +0800

    add tooltip

--- admin/helpers/summary_helper.rb
+++ admin/helpers/summary_helper.rb
@@ -1,8 +1,14 @@
 SchaefflerWechat::Admin.helpers do
 
   def summary auto_reply
+    summary = full_summary(auto_reply)
+    summary.length > 40 ? summary[0,40] + '...' : summary
+  end
+
+  def full_summary auto_reply
+
     summary = auto_reply.title if auto_reply.rtype == 'news'
     summary = auto_reply.description if auto_reply.rtype == 'text'
-    summary.length > 40 ? summary[0,40] + '...' : summary
+    summary
   end
 end
